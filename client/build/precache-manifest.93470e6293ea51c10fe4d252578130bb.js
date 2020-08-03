95ad4d50da6b9ba3940481586ae42970073ccc8c
Chris Donaldson <chrisddoaldson@hotmail.com>
2020-08-03 01:19:58 +0100

    Build

---
+++ client/build/precache-manifest.93470e6293ea51c10fe4d252578130bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd5799ff232a9683472c22408cf9313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ed2ca4f7ff62bf8d8a5",
+    "url": "/static/css/main.42b70bff.chunk.css"
+  },
+  {
+    "revision": "ae10428d3986ee820b6c",
+    "url": "/static/js/2.b7ced441.chunk.js"
+  },
+  {
+    "revision": "1ed2ca4f7ff62bf8d8a5",
+    "url": "/static/js/main.320b4698.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
